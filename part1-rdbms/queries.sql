c5d799daf82ca738358a7fff6ceb2ddde1714cb2
Mohammad Anas <101318780+anas-acct@users.noreply.github.com>
2026-03-22 15:26:17 +0530

    Update queries.sql

--- part1-rdbms/queries.sql
+++ part1-rdbms/queries.sql
@@ -1,16 +1,21 @@
+-- Q1
 SELECT * FROM customers;
 
+-- Q2
 SELECT * FROM products
 WHERE price > 500;
 
+-- Q3
 SELECT orders.order_id, customers.name, orders.order_date
 FROM orders
 JOIN customers ON orders.customer_id = customers.customer_id;
 
-SELECT SUM(quantity * price) AS total_sales
+-- Q4
+SELECT SUM(price) AS total_sales
 FROM orders
 JOIN products ON orders.product_id = products.product_id;
 
+-- Q5
 SELECT sales_reps.name, COUNT(orders.order_id) AS total_orders
 FROM orders
 JOIN sales_reps ON orders.sales_rep_id = sales_reps.sales_rep_id
