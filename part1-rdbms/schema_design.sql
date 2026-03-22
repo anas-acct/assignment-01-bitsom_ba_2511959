f5308eeddc08435caf188e14b3bdd708aca21e48
Mohammad Anas <101318780+anas-acct@users.noreply.github.com>
2026-03-22 15:00:50 +0530

    Update schema_design.sql

--- part1-rdbms/schema_design.sql
+++ part1-rdbms/schema_design.sql
@@ -1,1 +1,6 @@
-
+CREATE TABLE customers (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(100) NOT NULL,
+    customer_email VARCHAR(100) NOT NULL,
+    customer_city VARCHAR(50) NOT NULL
+);
